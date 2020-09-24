d5d9eb00481d4bc61001d06132c0194ea226452f
Milad Awad <millad.99.awad@gmail.com>
2020-09-24 21:23:37 +0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:facebook/config/palette.dart';
+import 'package:facebook/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutte Facebook UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
